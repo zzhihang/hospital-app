12e5400d38bec3106740ed3fe09314d24d8dad9b
zhuzhihang <zzhihang@hotmail.com>
2022-03-24 10:46:19 +0800

    111

---
+++ src/components/tag/tagList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="tag-list">
+        <van-tabs v-model="active" type="card">
+            <van-tab title="标签 1" v-for="(item, index) in 10" :key="index">内容 1</van-tab>
+        </van-tabs>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    import { Tab, Tabs } from 'vant';
+    Vue.use(Tab);
+    Vue.use(Tabs);
+    export default {
+        props: ['data'],
+        data() {
+            return {
+                active: ''
+            }
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+/deep/.van-tab{
+    background: rgba(153, 153, 153, 0.1);
+    margin-right: 10px;
+}
+    /deep/.van-tabs__nav--card .van-tab{
+        border: none !important;
+    }
+    /deep/.van-tabs__nav--card{
+        border: none !important;
+    }
+</style>
--- src/routes/my_topic/index.vue
+++ src/routes/my_topic/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="body">
         <div class="main-box">
             <h6>用户研究方法</h6>
             <p class="intro">用户体验设计是以用户为中心的一种设计手段，以用户需求为目标而进行的设计。</p>
@@ -19,47 +19,61 @@
                 </li>
             </ul>
         </div>
+
+        <tagList />
     </div>
 </template>
 
 <script>
-    export default {
+    import Vue from 'vue';
+    import tagList from "../../components/tag/tagList";
 
+    export default {
+        components: {
+            tagList
+        }
     }
 </script>
 
 <style lang="less" scoped>
-.main-box{
-    background: #FFFBF9;
-    box-shadow: 0 -1px 12px 0 rgba(153, 153, 153, 0.18);
-    border-radius: 36px 36px 0 0;
-    padding: 20px 15px;
-    font-size: 16px;
-    h6{
-        font-size: 18px;
-    }
-    .intro{
-        margin-top: 13px;
-        line-height: 24px;
-    }
-    ul{
-        border-top: 1px solid rgb(153 153 153 / 30%);
-        @flex-sb-center();
-        padding-top: 15px;
-        margin-top: 15px;
-        li{
-            border-right: 1px solid rgb(153 153 153 / 30%);
-            text-align: center;
-            flex: 1;font-size: 18px;
-            &:last-child{
-                border-right: none;
+    .main-box {
+        background: #FFFBF9;
+        box-shadow: 0 -1px 12px 0 rgba(153, 153, 153, 0.18);
+        border-radius: 36px 36px 0 0;
+        padding: 20px 15px;
+        font-size: 16px;
+
+        h6 {
+            font-size: 18px;
+        }
+
+        .intro {
+            margin-top: 13px;
+            line-height: 24px;
+        }
+
+        ul {
+            border-top: 1px solid rgb(153 153 153 / 30%);
+            @flex-sb-center();
+            padding-top: 15px;
+            margin-top: 15px;
+
+            li {
+                border-right: 1px solid rgb(153 153 153 / 30%);
+                text-align: center;
+                flex: 1;
+                font-size: 18px;
+
+                &:last-child {
+                    border-right: none;
+                }
+            }
+
+            p {
+                font-size: 12px;
+                color: #999999;
+                margin-top: 8px;
             }
         }
-        p{
-            font-size: 12px;
-            color: #999999;
-            margin-top: 8px;
-        }
     }
-}
 </style>
